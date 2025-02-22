9ca0d4fa780c079e3541c48e0f089413913922ca
win 10 <win 10@DESKTOP-H6HO6MQ>
2025-02-21 22:19:04 -0600

    Agregar archivos de proyecto.

---
+++ Operaciones por Pila/Services/Conversion.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace Operaciones_por_Pila.Services
+{
+    public static class Conversion
+    {
+
+        private static int Prioridad(char operador)
+        {
+            if (operador == '+' || operador == '-')
+                return 1;
+            if (operador == '*' || operador == '/')
+                return 2;
+            if (operador == '^')
+                return 3;
+            return 0;
+        }
+
+        public static string ConversionOP(string expresion)
+        {
+            Pila pila = new Pila(expresion.Length);
+            StringBuilder postfija = new StringBuilder();
+
+            foreach (char c in expresion)
+            {
+                if (char.IsLetterOrDigit(c))
+                {
+                    postfija.Append(c);
+                }
+                else if (c == '(')
+                {
+                    pila.Push(c);
+                }
+                else if (c == ')')
+                {
+                    while (!pila.IsEmpty && pila.Peek() != '(')
+                    {
+                        postfija.Append(pila.Pop());
+                    }
+                    if (!pila.IsEmpty) pila.Pop();
+                }
+                else
+                {
+                    while (!pila.IsEmpty && Prioridad(pila.Peek()) >= Prioridad(c))
+                    {
+                        postfija.Append(pila.Pop());
+                    }
+                    pila.Push(c);
+                }
+            }
+
+            while (!pila.IsEmpty)
+            {
+                postfija.Append(pila.Pop());
+            }
+
+            return postfija.ToString();
+        }
+
+        public static double Evaluar(string expresionPostfija)
+        {
+            Pila pila = new Pila(expresionPostfija.Length);
+
+            foreach (char c in expresionPostfija)
+            {
+                if (char.IsDigit(c)) 
+                {
+                    pila.Push(c);
+                }
+                else 
+                {
+                    double op2 = pila.Pop() - '0';
+                    double op1 = pila.Pop() - '0';
+                    double resultado = 0;
+
+                    switch (c)
+                    {
+                        case '+': resultado = op1 + op2; break;
+                        case '-': resultado = op1 - op2; break;
+                        case '*': resultado = op1 * op2; break;
+                        case '/': resultado = op1 / op2; break;
+                        case '^': resultado = Math.Pow(op1, op2); break;
+                    }
+
+                    pila.Push((char)(resultado + '0')); 
+                }
+            }
+
+            return pila.Pop() - '0'; 
+        }
+
+
+    }
+
+
+
+}
---
+++ Operaciones por Pila/Services/Pila.cs
@@ -0,0 +1,42 @@
+﻿namespace Operaciones_por_Pila.Services
+{
+    public class Pila
+    {
+        public char[] Elementos { get; set; }
+        public int TOPE { get; set; }
+
+        public Pila(int tamaño)
+        {
+            Elementos = new char[tamaño];
+            TOPE = 0;
+        }
+
+        public bool IsEmpty => TOPE == 0;
+        public bool IsFull => TOPE == Elementos.Length;
+
+        public void Push(char elemento)
+        {
+            if (IsFull) return;
+            Elementos[TOPE] = elemento;
+            TOPE++;
+        }
+
+        public char Pop()
+        {
+            if (IsEmpty)
+                return '\0';
+
+            TOPE--;
+            return Elementos[TOPE];
+        }
+
+        public char Peek()
+        {
+            if (IsEmpty)
+                return '\0';
+
+            return Elementos[TOPE - 1];
+        }
+
+    }
+}
